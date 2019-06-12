fbd320c8a9c635327e4cd8f8698335cc782a9fdc
Fenrir-019 <51722296+Fenrir-019@users.noreply.github.com>
2019-06-12 12:24:51 -0400

    algo and graph updated

--- Kumirei_graph.py
+++ Kumirei_graph.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 11 19:29:27 2019
+Created on Tue Jun 11 21:29:27 2121
 
 @author: -Fenrir
 """
@@ -10,17 +10,10 @@ import networkx as nx
 from matplotlib import pyplot as plt
 
 options = {'node_color': '#8fb2ea',
-           'node_size': 1200,
+           'node_size': 1210,
            'width': 1,
            'with_labels': True
            }
-
-
-def get_min_in_node(G):
-    """ Given a graph G, return the node with minimum number of in edges.
-    Return any of them if there are more than 1 such edges.
-    """
-    
 
 
 def create_graph():
@@ -30,41 +23,32 @@ def create_graph():
     G = nx.DiGraph()
     G.add_nodes_from(range(21))
     for i in range(21):
-#        if i % 5 == 0:
-        G.add_edge(i, (i+2) % 21)
-        G.add_edge(i, (i+4) % 21)
-        G.add_edge(i, (i+8) % 21)
-        G.add_edge(i, (i+22) % 21)
-#    G.add_node(21)
-    for i in range(21):
-        l = [i]
-        l.extend(G[i].keys())
-        for key in G[i].keys():
-            l.extend(G[key])
-        l.sort()
-        print i, l
+        G.add_edge(i, (4 * i + 1) % 21)
+        G.add_edge(i, (4 * i + 2) % 21)
+        G.add_edge(i, (4 * i + 3) % 21)
+        G.add_edge(i, (4 * i + 4) % 21)
+
+    # some manual fixes for nodes pointing towards itself    
+    G.remove_edge(6,6)
+    G.remove_edge(13,13)
+    G.remove_edge(20,20)
+    G.add_edge(6,13)
+    G.add_edge(13,20)
+    G.add_edge(20,6)
     
-    print G.in_edges()
-#    for i in range(21):
-#        for j in range(21):
-#            try:
-#                assert nx.shortest_path_length(G, i, j) <= 2
-#            except AssertionError:
-#                print i, j, nx.shortest_path_length(G, i, j)
-#                print G[i]
-#                for ele in G[i].keys():
-#                    print G[ele]
+    G = nx.relabel_nodes(G, lambda x: x + 1)
+    G.add_node(0)
+
     return G
 
 
 def plot_graph(G):
-    """ Given the graph created in create_graph(), divide the nodes into three
+    """ Given the graph created in create_graph(), divide the nodes into two
     shells and plot the graph.
     """
     plt.figure(figsize=(12,12))
-    nx.draw_circular(G, **options)
-#    nx.draw_shell(G, nlist=[[0], range(1,8), range(8,22)], **options)
-#    plt.savefig("Kumirei_graph.png", dpi=300)
+    nx.draw_shell(G, nlist=[[0], range(1,22)], **options)
+    plt.savefig("Kumirei_graph.png", dpi=300)
     plt.show()
 
 
