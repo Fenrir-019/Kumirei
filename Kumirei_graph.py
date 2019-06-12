ee01d3420da64562941e230b5130b38ff7b16bc5
Fenrir-019 <51722296+Fenrir-019@users.noreply.github.com>
2019-06-11 23:23:18 -0400

    Add files via upload

--- Kumirei_graph.py
+++ Kumirei_graph.py
@@ -16,25 +16,44 @@ options = {'node_color': '#8fb2ea',
            }
 
 
-def print_neighbors(G, i):
-    """ Given a graph and an index, print a dictionary containing the
-    neighbors it points toward.
+def get_min_in_node(G):
+    """ Given a graph G, return the node with minimum number of in edges.
+    Return any of them if there are more than 1 such edges.
     """
-    return G[i]
     
+
 
 def create_graph():
     """ Create a directed graph with 22 nodes. One of the nodes is isolated,
     and all the other nodes have 4 in edges and 4 out edges.
     """
     G = nx.DiGraph()
-    G.add_nodes_from(range(1,22))
-    for i in range(1, 22):
-        G.add_edge(i, i % 21 + 1)
-        G.add_edge(i, (i+1) % 21 + 1)
-        G.add_edge(i, (i+2) % 21 + 1)
-        G.add_edge(i, (i+3) % 21 + 1)
-    G.add_node(0)
+    G.add_nodes_from(range(21))
+    for i in range(21):
+#        if i % 5 == 0:
+        G.add_edge(i, (i+2) % 21)
+        G.add_edge(i, (i+4) % 21)
+        G.add_edge(i, (i+8) % 21)
+        G.add_edge(i, (i+22) % 21)
+#    G.add_node(21)
+    for i in range(21):
+        l = [i]
+        l.extend(G[i].keys())
+        for key in G[i].keys():
+            l.extend(G[key])
+        l.sort()
+        print i, l
+    
+    print G.in_edges()
+#    for i in range(21):
+#        for j in range(21):
+#            try:
+#                assert nx.shortest_path_length(G, i, j) <= 2
+#            except AssertionError:
+#                print i, j, nx.shortest_path_length(G, i, j)
+#                print G[i]
+#                for ele in G[i].keys():
+#                    print G[ele]
     return G
 
 
@@ -42,9 +61,10 @@ def plot_graph(G):
     """ Given the graph created in create_graph(), divide the nodes into three
     shells and plot the graph.
     """
-    plt.figure(figsize=(9,9))
-    nx.draw_shell(G, nlist=[[0], range(1,8), range(8,22)], **options)
-    plt.savefig("Kumirei_graph.eps")
+    plt.figure(figsize=(12,12))
+    nx.draw_circular(G, **options)
+#    nx.draw_shell(G, nlist=[[0], range(1,8), range(8,22)], **options)
+#    plt.savefig("Kumirei_graph.png", dpi=300)
     plt.show()
 
 
